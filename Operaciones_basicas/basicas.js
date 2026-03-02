70897ec7cb0168e297cbc66d9132a52abe47129c
´cmarlon´ <cmarlon483@gmail.com>
2026-03-01 20:11:54 -0600

    Segunda tarea

--- Operaciones_basicas/basicas.js
+++ Operaciones_basicas/basicas.js
@@ -1,4 +1,11 @@
-let a = 5;
-let b = 6;
+let a = parseInt(prompt("Ingresa el primer numero"));
+let b = parseInt(prompt("Ingresa el segundo numero"));
+let suma = a+b;
+let resta = a-b;
+let multiplicacion = a*b;
+let division = a/b;
 
-console.log(a+b);
+console.log("Tu suma da : " + suma );
+console.log("Tu resta da : " + resta );
+console.log("Tu multiplicacion da : " + multiplicacion );
+console.log("Tu division da : " + division );
